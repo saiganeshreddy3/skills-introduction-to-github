7d56238cef8236f4bac7cc78c53666bab7ece685
saiganeshreddy3 <skotte2@gitam.in>
2024-05-17 09:11:40 +0000

    nothing

---
+++ calculator.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int num1,num2;
+    int result;
+    printf("Enter two numbers=");
+    scanf("%d%d",&num1,&num2);
+    result=num1*num2;
+    printf("Multiplication of two numbers is %d",num1);
+    return 0;
+}
