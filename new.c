fec3bc3501da0ee3c84675919587a1d6887e4373
saiganeshreddy3 <skotte2@gitam.in>
2024-05-17 09:23:12 +0000

    removed result variable

---
+++ new.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int num1,num2;
+    printf("Enter two numbers=");
+    scanf("%d%d",&num1,&num2);
+    printf("Multiplication of two numbers is %d",num1*num2);
+    return 0;
+}
